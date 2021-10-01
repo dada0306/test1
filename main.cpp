326ff21cfd8ae9d6821b0139fb92371efaf8e537
dada0306 <91716043+dada0306@users.noreply.github.com>
2021-10-01 14:21:47 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "linkedstack.h"
+
+// ÁÖ ÇÔ¼ö
+int main(void)
+{
+	LinkedStackType s;
+	init(&s);
+	push(&s, 1); print_stack(&s);
+	push(&s, 2); print_stack(&s);
+	push(&s, 3); print_stack(&s);
+	pop(&s); print_stack(&s);
+	pop(&s); print_stack(&s);
+	pop(&s); print_stack(&s);
+	return 0;
+}
